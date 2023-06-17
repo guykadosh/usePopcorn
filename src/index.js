52f8f1d14ae7711241846a5844ae6495a3334ce2
Guy Kadosh <guyk50@gmail.com>
2023-06-17 12:24:19 +0300

    Moved components into seperate files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-import StarRating from './StarRating'
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating messages={['hi', 'bye', 'dye', 'guy', 'shay']} /> */}
   </React.StrictMode>
 )

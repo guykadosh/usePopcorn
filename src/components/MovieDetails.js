957421e5b0c5ba008a2b2515f738e4efcbf942fe
Guy Kadosh <guyk50@gmail.com>
2023-06-18 13:17:13 +0300

    effect for sideffect keydown event

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -66,6 +66,20 @@ export default function MovieDetails({
     }
   }, [title])
 
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === 'Escape') {
+        onCloseMovie()
+      }
+    }
+
+    document.addEventListener('keydown', callback)
+
+    return () => {
+      document.removeEventListener('keydown', callback)
+    }
+  }, [onCloseMovie])
+
   return (
     <div className="details">
       {isLoading ? (
